44a3857a4a68df80be6412031d8242b88c4202e8
boredwolf <felix.jouvhomme@gmail.com>
2021-11-01 16:25:00 +0100

    routeur

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import Home from './pages/Home';
-import Knowledges from './pages/Knowledges';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import NotFound from './pages/NotFound'
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Knowledges from "./pages/Knowledges";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/compétences" component={Knowledges} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
