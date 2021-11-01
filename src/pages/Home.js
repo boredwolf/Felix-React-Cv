3067fabd6c82796b15a3897d6a85a6537879fda9
boredwolf <felix.jouvhomme@gmail.com>
2021-11-01 12:51:22 +0100

    a

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            home c'est ici
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Knowledges.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Knowledges = () => {
+    return (
+        <div>
+            c'est knowledges
+        </div>
+    );
+};
+
+export default Knowledges;
